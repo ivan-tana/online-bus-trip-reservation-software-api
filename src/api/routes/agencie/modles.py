bfe59ed8f6beabf053cdc42d503cc4bfd7c33291
ivan-tana <ivan-tana@outlook.com>
2024-01-17 18:46:07 +0100

    json_schema_extra for models

--- src/api/routes/agencie/modles.py
+++ src/api/routes/agencie/modles.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel
-from pydantic import EmailStr
+from pydantic import BaseModel, Field
+from pydantic import EmailStr, AnyUrl
 from pydantic_extra_types.phone_numbers import PhoneNumber
 from api.config import SUPPORTED_COUNTRY
 
@@ -11,26 +11,104 @@ class AgencyCreationForm(BaseModel):
     contact_email: EmailStr 
     description: str 
     phone_number:  PhoneNumber
-    why_choose_us: str 
-    agency_imeages: list[str]
+    why_choose_us: str | None = None
+    agency_images: list[AnyUrl] | None = None
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": 
+            [
+                {
+                    "name": "Mogamo",
+                    "contact_email": "info@mogamo.com",
+                    "description": "The Best travel agency in cameroon",
+                    "phone_number": "+237678098765",
+                    "why_choose_us": "Best customer service",
+                    "agency_images": [
+                        "https://mogamo.com/images/park.png"
+                    ]           
+                }
+            ]
+
+        }
+    }
+
+class AgencyUpdateForm(BaseModel):
+    name: str | None = None
+    contact_email: EmailStr | None = None
+    description: str | None = None
+    phone_number:  PhoneNumber | None = None
+    why_choose_us: str | None = None
+    agency_images: list[AnyUrl] | None = None
 
 class Locatiion(BaseModel):
     country: str 
     region : str 
     city: str 
-    street: str 
-    geo_location: list
+    geo_location: list[float] | None = None
+    model_config = {
+        "json_schema_extra": {
+            "examples": 
+            [
+                {
+                    "country": "Mogamo",
+                    "region": "South West",
+                    "geo_location": [
+                        92.34, 123.32
+                    ],
+                    "city": "Kumba Town",       
+                }
+            ]
+
+        }
+    }
 
 class BranchCreation(BaseModel):
-    name: str | None 
+    agency_id: int 
+    name: str | None = None
     location : Locatiion 
     momo_phone_number: PhoneNumber
     description: str 
-    images: list
+    images: list[AnyUrl] | None = None
     email: EmailStr
     contact_phone_number: PhoneNumber
 
+    model_config = {
+        "json_schema_extra": {
+            "examples": 
+            [
+                {
+                    "agency_id": 1,
+                    "name": "Mogamo Kumba Branch",
+                    "location": {
+                    "country": "Cameroon",
+                    "region": "South west",
+                    "city": "Kumba Town",
+                    "geo_location": [
+                        92.34, 123.32
+                    ]
+                    },
+                    "momo_phone_number": "+237789087962",
+                    "description": "Mogamo kumba branch",
+                    "images": [
+                        "https://mogamo.com/images/park.png"
+                        ],
+                    "email": "kumba@mogamo.com",
+                    "contact_phone_number": "+237789087962"          
+                }
+            ]
 
+        }
+    }
+
+class BranchUpdate(BaseModel):
+    name: str | None = None
+    location : Locatiion | None = None
+    momo_phone_number: PhoneNumber | None = None
+    description: str | None = None
+    images: list[AnyUrl] | None = None
+    email: EmailStr | None = None
+    contact_phone_number: PhoneNumber | None = None
 
 
 
