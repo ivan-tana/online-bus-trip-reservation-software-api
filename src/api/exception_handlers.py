65e251c13195eb8450419ac38179fb4ad12c04ec
ivan-tana <ivan-tana@outlook.com>
2024-01-17 18:42:58 +0100

    request_validation_error: change default request error handler

--- src/api/exception_handlers.py
+++ src/api/exception_handlers.py
@@ -2,8 +2,10 @@ import logging
 
 from fastapi import FastAPI, status, Request
 from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
 from api.helpers import exceptions
 from api.helpers.functions import exception_message
+from pydantic_core import PydanticCustomError
 
 
 
@@ -24,9 +26,22 @@ async def agency_already_exist(_: Request, exception: exceptions.AgencyAlreadyEx
         content={'message': exception_message(exception)}
     )
 
+async def request_validation_error(_: Request, exception: RequestValidationError):
+
+    logging.exception(exception)
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content={
+            'value': exception_message(exception)[0]['input'],
+            'message': exception_message(exception)[0]['msg'],
+            'log': exception_message(exception)[0]['loc']
+            }
+    )
+
 
 def register_exception_handlers(app: FastAPI) -> None:
     """Add exception handlers to FastAPI app
     """
     app.add_exception_handler(Exception, default_error_handler)
     app.add_exception_handler(exceptions.AgencyAlreadyExist, agency_already_exist)
+    app.add_exception_handler(RequestValidationError, request_validation_error)
