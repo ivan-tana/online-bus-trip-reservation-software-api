8cc7803f6a195edb2801ac5ba3c906b786e17c28
ivan-tana <ivan-tana@outlook.com>
2024-01-19 22:44:35 +0100

    config

--- src/api/config.py
+++ src/api/config.py
@@ -1,1 +1,12 @@
+from pathlib import Path
+
 SUPPORTED_COUNTRY = ['CM']
+TORTOISE_ORM = {
+    "connections": {"default": "sqlite://src/.local_settings/database.sqlite3"},  # Example for SQLite
+    "apps": {
+        "models": {
+            "models": ['api.database.modles'],  # Path to your Tortoise models
+            "default_connection": "default",
+        },
+    },
+}
